de0253ae6045eda8108455c9cfce4eed28a2368e
Pranav Karthik <karthik.pranav88@gmail.com>
2026-03-21 19:45:14 -0500

    commit #2

--- test_tavily.py
+++
@@ -1,5 +0,0 @@
-﻿from tavily import TavilyClient
-
-client = TavilyClient(api_key="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")
-response = client.search("test query")
-print(response)
